57009a50b93a359417c1b0f7fc7284bfb409e0bf
JeffreyTangMQ46469702 <jeffrey.tang1@students.mq.edu.au>
2023-03-24 00:35:02 +1100

    More Physics (finally)
    Tried to adjust more physics to mixed results lol. (finally works)

--- Group E Project/Assets/Player.cs
+++ Group E Project/Assets/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     public float horizontalforce; 
     public bool grounded;
-
+    public bool ramped;
+    private float maxspeed = 8;
+    
     private Vector2 groundNormal = Vector2.up;
     public float maxRotationAngle = 45f;
     public float rotationSpeed;
@@ -35,36 +37,56 @@ public class Player : MonoBehaviour
     {
         LayerMask mask = LayerMask.GetMask("ground");
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 2, mask);
-        rb.AddForce(transform.right * horizontalforce * 20);
+        if (ramped == true)
+        {
+            rb.AddForce(transform.right * 7500);
+        }
+        if(rb.velocity.magnitude <= maxspeed)
+        {
+            rb.AddForce(transform.right * horizontalforce * 1000);
+        }
         //maintain vertical on slope (testing)
-        RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.down);
-        if (hit2.collider != null)
-        {
-            groundNormal = hit2.normal;
-        }
-        float angle = Vector2.Angle(Vector2.up, groundNormal);
-        if (angle > maxRotationAngle)
-        {
-            Quaternion targetRotation = Quaternion.FromToRotation(Vector2.up, groundNormal);
-            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
-        }
-        // tilted center of gravity
-        float tiltAngle = -horizontalforce * tiltSpeed;
-        tiltAngle = Mathf.Clamp(tiltAngle, -maxTiltAngle, maxTiltAngle);
-        transform.rotation = Quaternion.Euler(0f, 0f, tiltAngle);
+        // RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.down);
+        // if (hit2.collider != null)
+        // {
+        //     groundNormal = hit2.normal;
+        // }
+        // float angle = Vector2.Angle(Vector2.up, groundNormal);
+        // if (angle > maxRotationAngle)
+        // {
+        //     Quaternion targetRotation = Quaternion.FromToRotation(Vector2.up, groundNormal);
+        //     rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+        // }
+        // // tilted center of gravity
+        // float tiltAngle = -horizontalforce * tiltSpeed;
+        // tiltAngle = Mathf.Clamp(tiltAngle, -maxTiltAngle, maxTiltAngle);
+        // transform.rotation = Quaternion.Euler(0f, 0f, tiltAngle);
     }
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "ground")
+        if (collider.gameObject.tag == "ground" || collider.gameObject.tag == "ramp")
         {
             grounded = true;
         } 
+        if(collider.gameObject.tag == "ramp")
+        {
+            ramped = true;
+            //rb.freezeRotation = false;
+            //rb.rotation = 45;
+        }
     }
     void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "ground")
+        if (collider.gameObject.tag == "ground" || collider.gameObject.tag == "ramp")
         {
             grounded = false;
+        }
+        if(collider.gameObject.tag == "ramp")
+        {
+            ramped = false;
+            //rb.rotation = 0;
+            //rb.freezeRotation = false;
+            
         }
     }
 }
