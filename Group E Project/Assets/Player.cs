4d9599ee3ddfd6fb4d7ec28c7dac117a02f5adac
JeffreyTangMQ46469702 <jeffrey.tang1@students.mq.edu.au>
2023-03-27 21:53:19 +1100

    Revert "Merge branch 'main' into Jeff"
    This reverts commit d11d29a4054885e2d57efc348fe7690cfdaaa656, reversing
    changes made to 6800c3b4c43069fb8a61a43cbc83745115d7896c.

--- Group E Project/Assets/Player.cs
+++ Group E Project/Assets/Player.cs
@@ -8,110 +8,58 @@ public class Player : MonoBehaviour
     public float horizontalforce; 
     public bool grounded;
     public bool ramped;
-    public bool downramped;
-    private float maxspeed = 12f;
-    
-    private Vector2 groundNormal = Vector2.up;
-    public float maxRotationAngle = 45f;
-    public float rotationSpeed;
-
-    public float tiltSpeed = 20f;
-    public float maxTiltAngle = 45f;
-    private float horizontalInput;
-
+    private float maxspeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalforce = Input.GetAxis("Horizontal");
-       
         if (Input.GetButtonDown("Jump") && grounded == true) 
         {
-             rb.AddForce(transform.up * 84000);
+             rb.AddForce(transform.up * 170000);
         }
     }
     void FixedUpdate()
     {
-        
+        Debug.Log(rb.velocity.magnitude);
         LayerMask mask = LayerMask.GetMask("ground");
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 2, mask);
-        RaycastHit2D test = Physics2D.Raycast(transform.position, Vector2.down, 2, mask);
-
-        if (test.collider!= null)
-        {
-            grounded = true;
-        }
-        else 
-        {
-            grounded = false;
-        }
         
         if (ramped == true)
         {
-            rb.AddForce(transform.right * 7500);
-        }
-        if (downramped == true)
-        {
-            rb.AddForce(transform.right * 7500);
+            rb.AddForce(transform.right * 8000);
         }
         if(rb.velocity.magnitude <= maxspeed)
         {
-            rb.AddForce(transform.right * horizontalforce * 500);
+            rb.AddForce(transform.right * horizontalforce * 1000);
         }
-        //maintain vertical on slope (testing)
-        // RaycastHit2D hit2 = Physics2D.Raycast(transform.position, Vector2.down);
-        // if (hit2.collider != null)
-        // {
-        //     groundNormal = hit2.normal;
-        // }
-        // float angle = Vector2.Angle(Vector2.up, groundNormal);
-        // if (angle > maxRotationAngle)
-        // {
-        //     Quaternion targetRotation = Quaternion.FromToRotation(Vector2.up, groundNormal);
-        //     rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
-        // }
-        // // tilted center of gravity
-        // float tiltAngle = -horizontalforce * tiltSpeed;
-        // tiltAngle = Mathf.Clamp(tiltAngle, -maxTiltAngle, maxTiltAngle);
-        // transform.rotation = Quaternion.Euler(0f, 0f, tiltAngle);
+        
     }
     void OnTriggerEnter2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "ground" || collider.gameObject.tag == "ramp" || collider.gameObject.tag == "downramp")
+        if (collider.gameObject.tag == "ground" || collider.gameObject.tag == "ramp")
         {
-           
+            grounded = true;
         } 
-        if(collider.gameObject.tag == "ramp" || collider.gameObject.tag == "downramp")
+        if(collider.gameObject.tag == "ramp")
         {
             ramped = true;
-            //rb.freezeRotation = false;
-            //rb.rotation = 45;
-        }
-        if (collider.gameObject.tag == "downramp")
-        {
-          downramped = true;
         }
     }
     void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.gameObject.tag == "ground" || collider.gameObject.tag == "ramp" || collider.gameObject.tag == "downramp")
+        if (collider.gameObject.tag == "ground" || collider.gameObject.tag == "ramp")
         {
-            
+            grounded = false;
         }
-        if(collider.gameObject.tag == "ramp" || collider.gameObject.tag == "downramp")
+        if(collider.gameObject.tag == "ramp")
         {
             ramped = false;
-            //rb.rotation = 0;
-            //rb.freezeRotation = false;
-        }
-        if (collider.gameObject.tag == "downramp")
-        {
-          downramped = false;
         }
     }
 }
