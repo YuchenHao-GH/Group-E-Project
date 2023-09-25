1e5255415238a85dbeae91c5203451314c2ddd46
YuchenHao-GH <brucehao35006758@gmail.com>
2023-09-25 11:06:32 +1000

    update snowball

--- Group E Project/Assets/Scripts/FollowPlayer.cs
+++ Group E Project/Assets/Scripts/FollowPlayer.cs
@@ -6,9 +6,13 @@ public class FollowPlayer : MonoBehaviour
 {
     public float speed = 5f;
     private Transform player;
-    public float updateInterval = 1.0f;
+    public float updateInterval = 1.0f; 
     private float timeSinceLastUpdate = 0.0f;
     private Vector3 targetPosition;
+    private Rigidbody2D rb;
+
+    private bool isActive = false;
+    public float activationDelay = 2.0f;
 
     private void Start()
     {
@@ -17,10 +21,27 @@ public class FollowPlayer : MonoBehaviour
         {
             targetPosition = player.position;
         }
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+
+        StartCoroutine(ActivateSnowball());
+    }
+
+    private IEnumerator ActivateSnowball()
+    {
+        yield return new WaitForSeconds(activationDelay);
+        isActive = true;
+        rb.isKinematic = false;
     }
 
     private void Update()
     {
+        if (!isActive)
+        {
+            return;
+        }
+
         if (player != null)
         {
             timeSinceLastUpdate += Time.deltaTime;
@@ -31,15 +52,20 @@ public class FollowPlayer : MonoBehaviour
             }
 
             Vector3 direction = (targetPosition - transform.position).normalized;
-            transform.position += direction * speed * Time.deltaTime;
+            rb.AddForce(direction * speed * Time.deltaTime);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.CompareTag("Player"))
+        if (!isActive)
         {
-            Player player = other.GetComponent<Player>();
+            return;
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
             if (player != null)
             {
                 float damage = 20.0f;
