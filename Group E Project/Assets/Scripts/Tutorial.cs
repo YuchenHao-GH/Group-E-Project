f560051a3fd06ed6b6e2063aac98134ad011c5fa
JeffreyTangMQ46469702 <jeffrey.tang1@students.mq.edu.au>
2023-10-25 12:30:05 +1100

    Put in some screenshots

--- Group E Project/Assets/Scripts/Tutorial.cs
+++ Group E Project/Assets/Scripts/Tutorial.cs
@@ -13,6 +13,10 @@ public class Tutorial : MonoBehaviour
     public GameObject Text2;
     public GameObject Text3;
     public GameObject Text4;
+    public GameObject Image1;
+    public GameObject Image2;
+    public GameObject Image3;
+    public GameObject Image4;
     public int Textcounter = 1;
     // Start is called before the first frame update
     void Start()
@@ -49,21 +53,29 @@ public class Tutorial : MonoBehaviour
         Text2.SetActive(false);
         Text3.SetActive(false);
         Text4.SetActive(false);
+        Image1.SetActive(false);
+        Image2.SetActive(false);
+        Image3.SetActive(false);
+        Image4.SetActive(false);
         Textcounter--;
        
         switch (Textcounter)
         {
             case 1:
                 Text1.SetActive(true);
+                Image1.SetActive(true);
                 break;
             case 2:
                 Text2.SetActive(true);
+                Image2.SetActive(true);
                 break;
             case 3:
                 Text3.SetActive(true);
+                Image3.SetActive(true);
                 break;
             case 4:
                 Text4.SetActive(true);
+                Image4.SetActive(true);
                 break;
         }
     }
@@ -74,21 +86,29 @@ public class Tutorial : MonoBehaviour
         Text2.SetActive(false);
         Text3.SetActive(false);
         Text4.SetActive(false);
+        Image1.SetActive(false);
+        Image2.SetActive(false);
+        Image3.SetActive(false);
+        Image4.SetActive(false);
         Textcounter++;
        
         switch (Textcounter)
         {
             case 1:
                 Text1.SetActive(true);
+                Image1.SetActive(true);
                 break;
             case 2:
                 Text2.SetActive(true);
+                Image2.SetActive(true);
                 break;
             case 3:
                 Text3.SetActive(true);
+                Image3.SetActive(true);
                 break;
             case 4:
                 Text4.SetActive(true);
+                Image4.SetActive(true);
                 break;
         }
     }
